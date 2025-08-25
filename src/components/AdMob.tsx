a03752a89313b0c63455b6088c01437d8d5a5965
Rally Live <rallylivenet@gmail.com>
2025-08-25 06:39:06 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/components/AdMob.tsx
+++ src/components/AdMob.tsx
@@ -16,21 +16,26 @@ interface AdMobProps {
 const AdMob = ({ adSlot }: AdMobProps) => {
   useEffect(() => {
     try {
-      (window.adsbygoogle = window.adsbygoogle || []).push({});
+      if (typeof window !== 'undefined') {
+        (window.adsbygoogle = window.adsbygoogle || []).push({});
+      }
     } catch (err) {
       console.error(err);
     }
-  }, []);
+  }, [adSlot]);
 
   return (
-    <ins
-      className="adsbygoogle"
-      style={{ display: 'block', width: '100%' }}
-      data-ad-client="ca-pub-3358665652492622"
-      data-ad-slot={adSlot}
-      data-ad-format="auto"
-      data-full-width-responsive="true"
-    />
+    <div key={adSlot}>
+        <ins
+        className="adsbygoogle"
+        style={{ display: 'block', width: '100%' }}
+        data-ad-client="ca-pub-3358665652492622"
+        data-ad-slot={adSlot}
+        data-ad-format="auto"
+        data-full-width-responsive="true"
+        data-ad-status="unfilled"
+        />
+    </div>
   );
 };
 
